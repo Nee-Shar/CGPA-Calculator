0b4ed7a9df92b7dfb486beaf0710a158aa13ac85
Neeraj Sharma <99169026+Nee-Shar@users.noreply.github.com>
2024-06-07 22:02:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ document.getElementById("cal").addEventListener("click", () => {
   var sem3 = document.getElementById("sem3").value;
   var sem4 = document.getElementById("sem4").value;
   var sem5=document.getElementById("sem5").value;
+   var sem6=document.getElementById("sem6").value;
   var drop = document.getElementById("drop").value;
-  var sum = sem1 * 20 + sem2 * 24 + sem3 * 20 + sem4 * 20 +sem5*24;
-  var sub = drop * 4;
+  
+  var drop2= document.getElementById("drop2").value;
+  var sum = sem1 * 20 + sem2 * 24 + sem3 * 20 + sem4 * 20 +sem5*24 +sem6*24;
+  var sub = drop * 4+drop2*4;
 
   //console.log(sum - sub);
   if(sub!=0)
-    var ans = (sum - sub) / 104;
+    var ans = (sum - sub) / 132;
   else
-    var ans=(sum)/108;
+    var ans=(sum)/132;
   //console.log(ans);
   document.getElementById("final").value = ans;
   document.getElementById("final").style.visibility = "visible";
